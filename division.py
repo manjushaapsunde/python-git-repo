a180a13dfbbb83198293afd9e80e4f8293a14b6e
manjushaapsunde <manjuapsunde04@gmail.com>
2022-10-31 23:42:32 +0530

    Division code added

---
+++ division.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+ans = num1 + num2
+print(f"Addition of {num1} and {num2} is:{ans}")
+
+sub = num1 - num2
+print(f"Substraction of {num1} and {num2} is:{sub}")
+
+mul = num1 * num2
+print(f"Multiplication of {num1} and {num2} is:{mul}")
+
+div = num1 / num2
+print(f"Division of {num1} and {num2} is:{div}")
